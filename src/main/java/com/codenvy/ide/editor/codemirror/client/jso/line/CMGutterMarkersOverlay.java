b943901a25d9719cfa22754449c0432c5fdcbfd2
Mickaël Leduque <mickael.leduque@serli.com>
2014-09-11 12:01:23 +0200

    Use a specific return type for lineinfo gutter markers

---
+++ src/main/java/com/codenvy/ide/editor/codemirror/client/jso/line/CMGutterMarkersOverlay.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.ide.editor.codemirror.client.jso.line;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayString;
+
+import elemental.dom.Element;
+
+/**
+ * The map of gutter markers for a line, included in the result of lineInfo().
+ */
+public class CMGutterMarkersOverlay extends JavaScriptObject {
+
+    protected CMGutterMarkersOverlay() {
+    }
+
+    /**
+     * Returns the marker for the given gutter.
+     * 
+     * @param gutterId the gutter
+     * @return the marker element
+     */
+    public final native Element getMarker(String gutterId) /*-{
+        return this[gutterid];
+    }-*/;
+
+    /**
+     * Returns the list of gutters.
+     * 
+     * @return the gutters
+     */
+    public final native JsArrayString getIds() /*-{
+        return this.getOwnPropertyNames();
+    }-*/;
+}
