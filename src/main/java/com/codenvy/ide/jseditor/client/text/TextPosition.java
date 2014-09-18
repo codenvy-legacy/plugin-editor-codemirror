c932e8c2fa3817f0ddded862cd206f727651afe3
Mickaël Leduque <mickael.leduque@serli.com>
2014-09-18 17:23:31 +0200

    Add toString, equals and hashcode to TextPosition and Textrange

--- src/main/java/com/codenvy/ide/jseditor/client/text/TextPosition.java
+++ src/main/java/com/codenvy/ide/jseditor/client/text/TextPosition.java
@@ -32,4 +32,39 @@ public class TextPosition {
     public int getCharacter() {
         return character;
     }
+
+    @Override
+    public String toString() {
+        return "(" + line + ", " + character + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + character;
+        result = prime * result + line;
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof TextPosition)) {
+            return false;
+        }
+        final TextPosition other = (TextPosition)obj;
+        if (character != other.character) {
+            return false;
+        }
+        if (line != other.line) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/com/codenvy/ide/jseditor/client/text/TextRange.java
+++ src/main/java/com/codenvy/ide/jseditor/client/text/TextRange.java
@@ -33,4 +33,47 @@ public class TextRange {
     public TextPosition getTo() {
         return this.to;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((from == null) ? 0 : from.hashCode());
+        result = prime * result + ((to == null) ? 0 : to.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof TextRange)) {
+            return false;
+        }
+        final TextRange other = (TextRange)obj;
+        if (from == null) {
+            if (other.from != null) {
+                return false;
+            }
+        } else if (!from.equals(other.from)) {
+            return false;
+        }
+        if (to == null) {
+            if (other.to != null) {
+                return false;
+            }
+        } else if (!to.equals(other.to)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TextRange [from=" + from + ", to=" + to + "]";
+    }
 }
