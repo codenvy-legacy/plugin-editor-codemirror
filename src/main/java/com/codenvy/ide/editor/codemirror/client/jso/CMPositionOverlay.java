30ed8e46abe46e16f87f9f247f55b31f3d06a445
Mickaël Leduque <mickael.leduque@serli.com>
2014-09-22 14:39:05 +0200

    CMPositionOverlay: missing javadoc

--- src/main/java/com/codenvy/ide/editor/codemirror/client/jso/CMPositionOverlay.java
+++ src/main/java/com/codenvy/ide/editor/codemirror/client/jso/CMPositionOverlay.java
@@ -12,18 +12,35 @@ package com.codenvy.ide.editor.codemirror.client.jso;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * A line+character position in the text.
+ */
 public class CMPositionOverlay extends JavaScriptObject {
     protected CMPositionOverlay() {
     }
 
-    public final native int getLine()/*-{
+    /**
+     * Returns the line part.
+     * @return the line
+     */
+    public final native int getLine() /*-{
         return this.line;
     }-*/;
 
-    public final native int getCharacter()/*-{
+    /**
+     * Returns the character part.
+     * @return the character
+     */
+    public final native int getCharacter() /*-{
         return this.ch;
     }-*/;
 
+    /**
+     * Creates a new instance of position.
+     * @param line the line part of the position
+     * @param character the character part of the position
+     * @return the position instance
+     */
     public static final native CMPositionOverlay create(int line, int character) /*-{
         var result = {};
         result.line = line;
