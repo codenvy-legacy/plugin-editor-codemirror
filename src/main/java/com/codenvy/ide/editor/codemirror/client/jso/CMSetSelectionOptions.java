0334d39d4881837cf6d18c07fef9a31699a06dd9
Mickaël Leduque <mickael.leduque@serli.com>
2014-11-26 15:45:43 +0100

    Add missing javadoc

--- src/main/java/com/codenvy/ide/editor/codemirror/client/jso/CMSetSelectionOptions.java
+++ src/main/java/com/codenvy/ide/editor/codemirror/client/jso/CMSetSelectionOptions.java
@@ -12,8 +12,12 @@ package com.codenvy.ide.editor.codemirror.client.jso;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * Option objects for set selection methods.
+ */
 public class CMSetSelectionOptions extends JavaScriptObject {
 
+    /** JSO mandated protected constructor. */
     protected CMSetSelectionOptions() {
     }
 
@@ -21,23 +25,41 @@ public class CMSetSelectionOptions extends JavaScriptObject {
      * Should we scroll to the selection head ? Default to true
      */
 
+    /**
+     * Tells if we should scroll to the selection head.
+     * @return false to not scroll
+     */
     public final native boolean getScroll() /*-{
         return this.scroll;
     }-*/;
 
+    /**
+     * Sets the flag that tells if we should scroll to the selection head.<br>
+     * Defaults to true.
+     * @param true to scroll
+     */
     public final native void setScroll(boolean newValue) /*-{
         this.scroll = newValue;
     }-*/;
 
     /*
-     * Detemines whether the selection history event may be merged with the previous one
+     * Determines whether the selection history event may be merged with the previous one
      */
 
-
+    /**
+     * Determines whether the selection history event may be merged with the previous one.
+     * @see https://codemirror.net/doc/manual.html#setSelection for the meaning of the values
+     * @return true if the history event can be merged
+     */
     public final native boolean getOrigin() /*-{
         return this.origin;
     }-*/;
 
+    /**
+     * Decides if the selection history event may be merged with the previous one.
+     * @see https://codemirror.net/doc/manual.html#setSelection for the meaning of the values
+     * @param newValue the new value
+     */
     public final native void setOrigin(String newValue) /*-{
         this.origin = newValue;
     }-*/;
@@ -47,18 +69,35 @@ public class CMSetSelectionOptions extends JavaScriptObject {
      * the old selection
      */
 
-    public final native boolean getBias() /*-{
+    /**
+     * Returns the adjustement when scrolling if the range is atomic.
+     * @return the adjustement
+     */
+    public final native int getBias() /*-{
         return this.bias;
     }-*/;
 
+    /**
+     * Sets the adjustement when scrolling if the range is atomic.<br>
+     * Valid values are 1 and -1 (resp. forward and backward)
+     * @param newValue the new value
+     */
     public final native void setBias(int newValue) /*-{
         this.bias = newValue;
     }-*/;
 
+    /**
+     * Creates a new option objects with the default values : scroll.
+     * @return a default option object
+     */
     public static final native CMSetSelectionOptions create() /*-{
         return {};
     }-*/;
 
+    /**
+     * Creates a new option objects with the 'no scroll' option set.
+     * @return a default option object
+     */
     public static final native CMSetSelectionOptions createNoScroll() /*-{
         return {"scroll": false};
     }-*/;
